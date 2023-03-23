f55b205d2e931b58d81ba29390bdbae62288e55b
Tymber Hamilton <tymberk@Tymbers-MacBook-Pro.local>
2023-03-23 00:07:56 -0500

    added queries to schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -58,9 +58,22 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
--- Made sure tables were imported
+-- view imported tables
 
 SELECT * FROM campaign;
 SELECT * FROM category;
 SELECT * FROM contacts;
 SELECT * FROM subcategory;
+
+-- Query each table.
+
+SELECT * FROM campaign
+	where (currency) = 'USD' AND (outcome) = 'successful';
+---------------------------------------------------------------
+SELECT (category_id) FROM category;
+---------------------------------------------------------------
+SELECT * FROM contacts
+	where (email) LIKE '%gmail%';
+---------------------------------------------------------------
+SELECT * FROM subcategory
+	where (subcategory_id) = 'subcat3';
